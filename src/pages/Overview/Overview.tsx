d3fd07f4fe06946a75e703d7ab79f81eb8420fb2
Mahmood El-Mahalawy <mxhmoodem@hotmail.com>
2026-01-15 15:53:41 +0000

    Create key information component

--- src/pages/Overview/Overview.tsx
+++ src/pages/Overview/Overview.tsx
@@ -1,8 +1,13 @@
+import { InformationButton } from '../../components/common/InformationButton/InformationButton';
+
 export default function Overview() {
   return (
     <div>
       {' '}
-      <h1>Overview</h1>{' '}
+      <h2>
+        Overview
+        <InformationButton tooltip="More information" ariaLabel="Information" />
+      </h2>{' '}
     </div>
   );
 }
