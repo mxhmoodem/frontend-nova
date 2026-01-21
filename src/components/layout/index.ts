14f512839feba4438fb88418f51c3b83ac7d282c
Travk33 <travis.kerali@yahoo.co.uk>
2026-01-21 18:33:19 +0000

    remove corner pattern becasue we are only implementing info modal

--- src/components/layout/index.ts
+++ src/components/layout/index.ts
@@ -1,5 +1,4 @@
 export { default as DashboardLayout } from './DashboardLayout';
 export { default as Header } from './Header';
 export { default as Sidebar } from './Sidebar';
-export { default as CornerPattern } from './CornerPattern';
 export { ProtectedRoute } from './ProtectedRoute';
