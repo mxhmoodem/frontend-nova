1d03f968210a89fdbd27e974862a5167a01e6c04
Mahmood El-Mahalawy <mxhmoodem@hotmail.com>
2026-01-23 12:16:24 +0000

    test fix

--- src/pages/AIConsole/AIConsole.test.tsx
+++ src/pages/AIConsole/AIConsole.test.tsx
@@ -1,11 +1,16 @@
 import { describe, it } from 'vitest';
 import { render, screen } from '@testing-library/react';
 import { expect } from 'vitest';
+import { AuthProvider } from '../../context/AuthContext/AuthContext';
 import AIConsole from './AIConsole';
 
 describe('AIConsole', () => {
   it('renders', () => {
-    render(<AIConsole />);
+    render(
+      <AuthProvider>
+        <AIConsole />
+      </AuthProvider>
+    );
     expect(screen.getByText('AI Console')).toBeDefined();
   });
 });
